9da0e56e5506b880f17e5edae530bc324caf2bf6
Vladimir <anykey196@gmail.com>
2025-04-03 20:20:05 +0500

    Swagger

--- src/main/java/ru/savelevvn/AuthApplication.java
+++ src/main/java/ru/savelevvn/AuthApplication.java
@@ -2,7 +2,9 @@ package ru.savelevvn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
