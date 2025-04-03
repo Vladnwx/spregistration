369506eee93b25d7d0ee803b4f91c87a9acd80fb
Vladimir <anykey196@gmail.com>
2025-04-03 16:49:30 +0500

    ADD Exception

---
+++ src/main/java/ru/savelevvn/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package ru.savelevvn.exception;
+
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
+    public ResponseEntity<String> handleNotFoundException(ChangeSetPersister.NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
